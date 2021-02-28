47ca27016d6c04422de9700a8b45be91ce967e12
Saiful islam <saifulkapasia23@gmail.com>
2021-02-28 23:34:41 +0600

    How to write and destructure usestate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const nayoks = ['Jasim', 'Shakib khan', 'Dibjol']
   return (
     <div className="App">
+      <MovieCounter></MovieCounter>
     <Nayok name={nayoks[0]}></Nayok>
     <Nayok name="Shakil"></Nayok>
     <Nayok name={nayoks[1]}></Nayok>
@@ -17,6 +19,17 @@ function App() {
       </header>
     </div>
   );
+}
+
+function MovieCounter() {
+  const [count, setCount] = useState(0);
+  console.log(count);
+  return (
+    <div>
+      <button>Add Movie</button>
+      <h3>Number of movies: </h3>
+    </div>
+  )
 }
 
 function Nayok(props) {
